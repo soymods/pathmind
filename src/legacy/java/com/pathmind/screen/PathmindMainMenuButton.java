53ee224235871e52b32375e265c1367a9510f302
Holden Thomas <holdenthomas@ryders-mbp.lan>
2026-02-13 22:01:39 -0700

    Add custom button background styling

--- src/legacy/java/com/pathmind/screen/PathmindMainMenuButton.java
+++ src/legacy/java/com/pathmind/screen/PathmindMainMenuButton.java
@@ -15,6 +15,11 @@ import com.pathmind.util.TextCompatibilityBridge;
 public class PathmindMainMenuButton extends ButtonWidget {
     private static final Identifier ICON_TEXTURE = PathmindMod.id("textures/gui/button_logo.png");
     private static final int ICON_PADDING = 2;
+    private static final int BUTTON_FILL = 0xFF1F232C;
+    private static final int BUTTON_HOVER = 0xFF2A2F3A;
+    private static final int BUTTON_DISABLED = 0xFF252A33;
+    private static final int BORDER_LIGHT = 0xFF3A3F4C;
+    private static final int BORDER_DARK = 0xFF000000;
     private static final String OPEN_EDITOR_KEY = "gui.pathmind.open_editor";
     private static final String OPEN_EDITOR_FALLBACK = "Open Pathmind Editor";
 
@@ -26,7 +31,7 @@ public class PathmindMainMenuButton extends ButtonWidget {
     @Override
     protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
         this.setTooltip(Tooltip.of(resolveOpenEditorText()));
-        super.renderWidget(context, mouseX, mouseY, delta);
+        drawButtonBackground(context);
 
         int iconSize = this.width - ICON_PADDING * 2;
         int iconX = this.getX() + ICON_PADDING;
@@ -48,6 +53,29 @@ public class PathmindMainMenuButton extends ButtonWidget {
         int color = (alphaComponent << 24) | rgb;
 
         GuiTextureRenderer.drawIcon(context, ICON_TEXTURE, iconX, iconY, iconSize, color);
+    }
+
+    private void drawButtonBackground(DrawContext context) {
+        int baseFill = !this.active ? BUTTON_DISABLED : (this.isHovered() ? BUTTON_HOVER : BUTTON_FILL);
+        int fill = applyAlpha(baseFill, this.alpha);
+        int topBorder = applyAlpha(BORDER_LIGHT, this.alpha);
+        int bottomBorder = applyAlpha(BORDER_DARK, this.alpha);
+
+        int x = this.getX();
+        int y = this.getY();
+        int w = this.width;
+        int h = this.height;
+        context.fill(x, y, x + w, y + h, fill);
+        context.fill(x, y, x + w, y + 1, topBorder);
+        context.fill(x, y, x + 1, y + h, topBorder);
+        context.fill(x, y + h - 1, x + w, y + h, bottomBorder);
+        context.fill(x + w - 1, y, x + w, y + h, bottomBorder);
+    }
+
+    private static int applyAlpha(int color, float alphaMultiplier) {
+        int alpha = (color >>> 24) & 0xFF;
+        int scaledAlpha = MathHelper.ceil(alpha * Math.max(0.0f, Math.min(1.0f, alphaMultiplier)));
+        return (scaledAlpha << 24) | (color & 0x00FFFFFF);
     }
 
     @Override
